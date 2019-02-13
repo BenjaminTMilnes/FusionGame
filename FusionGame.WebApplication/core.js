baf922547ac1b6db2233129485492ab2627bb793
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-02-13 20:06:25 +0000

    Setting up more core code

--- FusionGame.WebApplication/core.js
+++ FusionGame.WebApplication/core.js
@@ -1,1 +1,38 @@
-﻿
+﻿var game = new Game("gameView");
+var lastDraw = 0;
+
+window.addEventListener("keydown", function (e) {
+    game.keyDown(e);
+});
+
+window.addEventListener("keyup", function (e) {
+    game.keyUp(e);
+});
+
+window.addEventListener("resize", function (e) {
+    game.resize(e);
+});
+
+function startGame() {
+    game.initialise();
+
+    requestAnimationFrame(mainLoop);
+}
+
+function mainLoop(timestamp) {
+    var timeDelta = timestamp - lastDraw;
+
+    if (timeDelta < 30) {
+        requestAnimationFrame(mainLoop);
+        return;
+    }
+
+    game.update(timeDelta);
+    game.draw();
+
+    lastDraw = timestamp;
+
+    requestAnimationFrame(mainLoop);
+}
+
+window.addEventListener("DOMContentLoaded", startGame);
--- FusionGame.WebApplication/game.js
+++ FusionGame.WebApplication/game.js
@@ -1,1 +1,39 @@
 ﻿
+
+
+
+
+
+
+class Game {
+    constructor(canvasId) {
+        this.canvasId = canvasId;
+        this.entities = [];
+
+        this.time = 0;
+    }
+
+    initialise() {
+
+    }
+
+    update(timeDelta) {
+        this.time += timeDelta;
+    }
+
+    draw() {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
